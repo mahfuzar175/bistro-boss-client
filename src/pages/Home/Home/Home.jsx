a908c4ba40183e0df745fd7295954fb36daca010
mahfuzar175 <mahfuzar175@gmail.com>
2023-11-10 15:15:15 +0600

    added Featured section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner";
 import BannerB2 from "../Banner2/BannerB2";
 import Category from "../Category/Category";
 import PopularMenu from "../PopularMenu/PopularMenu";
+import Featured from "./Featured/Featured";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Category></Category>
             <BannerB2></BannerB2>
             <PopularMenu></PopularMenu>
+            <Featured></Featured>
         </div>
     );
 };
